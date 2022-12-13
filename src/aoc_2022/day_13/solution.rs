5b237881beedfb1b47cf4d244bce8f4483a241ee
jamesz96 <jamesz96.au@gmail.com>
2022-12-13 22:01:05 +1100

    problem 13 wip

---
+++ src/aoc_2022/day_13/solution.rs
@@ -0,0 +1,153 @@
+use std::{io::{BufReader, Error, BufRead}, fs::File};
+use crate::{aoc_2022::constants, util::file::get_current_dir};
+
+const PROBLEM: &str = "day_13";
+
+#[derive(Debug)]
+enum Element {
+    Value(i32),
+    Packet(Packet),
+}
+
+#[derive(Debug)]
+struct Packet {
+    elements: Vec<Element>,
+}
+
+fn is_in_order(a: &Packet, b: &Packet) -> bool {
+    let mut idx_a = 0;
+    let mut idx_b = 0;
+
+    while idx_a < a.elements.len() && idx_b < b.elements.len() {
+        let element_a = &a.elements[idx_a];
+        let element_b = &b.elements[idx_b];
+
+        match element_a {
+            Element::Value(val_a) => {
+                match element_b {
+                    Element::Value(val_b) => {
+                        if val_a > val_b { return false; }
+                        if val_a < val_b { return true; }
+                    },
+                    Element::Packet(packet_b) => {
+                        let transformed_packet_a = Packet { elements: vec![Element::Value(*val_a)] };
+                        let result = is_in_order(&transformed_packet_a, packet_b);
+                        if !result { return false; }
+                    },
+                };
+            },
+            Element::Packet(packet_a) => {
+                match element_b {
+                    Element::Value(val_b) => {
+                        println!("TRANSFORM");
+                        let transformed_packet_b = Packet { elements: vec![Element::Value(*val_b)] };
+                        let result = is_in_order(packet_a, &transformed_packet_b);
+                        if !result { return false; }
+                    },
+                    Element::Packet(packet_b) => {
+                        let result = is_in_order(packet_a, packet_b);
+                        if !result { return false; }
+                    }
+                };
+            }
+        };
+
+        idx_a += 1;
+        idx_b += 1;
+    }
+
+    if idx_a < a.elements.len() && idx_b == b.elements.len() {
+        return false
+    }
+    return true;
+}
+
+fn parse_packet(line: &str) -> Packet {
+    let mut root = Packet { elements: vec![] };
+    let mut stack: Vec<Packet> = Vec::new();
+
+    for char in line.chars() {
+        match char {
+            ',' => continue,
+            '[' => {
+                let new_packet = Packet { elements: vec![] };
+                stack.push(new_packet);
+            },
+            ']' => {
+                let packet = stack.pop().unwrap();
+                match stack.last_mut() {
+                    Some(parent) => {
+                        let packet_element = Element::Packet(packet);
+                        parent.elements.push(packet_element)
+                    },
+                    None => root = packet,
+                };
+            },
+            _ => {
+                let parent = stack.last_mut().unwrap();
+
+                let value = String::from(char).parse::<i32>().unwrap();
+                let value_element = Element::Value(value);
+
+                parent.elements.push(value_element);
+            }
+        }
+    }
+
+    return root;
+}
+
+
+/// Distress Signal
+/// https://adventofcode.com/2022/day/13
+pub fn solve(filename: String) -> Result<i32, Error> {
+    let current_dir = get_current_dir();
+    let path = format!("{}/src/{}/{}/{}", current_dir, constants::YEAR.to_string(), PROBLEM.to_string(), filename);
+
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    let mut result = 0;
+
+    let mut idx = 1;
+    let mut iter = reader.lines();
+
+    while let Some(line) = iter.next() {
+        match line {
+            Ok(input) => {
+                let packet_a = parse_packet(&input);
+                let packet_b = parse_packet(&iter.next().unwrap().unwrap());
+
+                if is_in_order(&packet_a, &packet_b) {
+                    println!("RIGHT ORDER");
+                    result += idx;
+                    println!("{}", idx);
+                }
+
+                // Skip blank line
+                iter.next();
+
+                idx += 1;
+            },
+            Err(error) => panic!("{}", error),
+        };
+    }
+
+    return Ok(result);
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::aoc_2022::day_13::solution::solve;
+
+    #[test]
+    fn part_1() {
+        let input_file = "sample.txt";
+        let result = solve(input_file.to_string());
+        match result {
+            Ok(x) => assert_eq!(x, 13),
+            Err(_error) => return,
+        }
+    }
+}
