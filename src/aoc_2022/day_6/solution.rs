ae4168be868d5727889f9952280d36284783381a
jamesz96 <jamesz96.au@gmail.com>
2022-12-15 08:09:14 +1100

    problem 6 - part 2

--- src/aoc_2022/day_6/solution.rs
+++ src/aoc_2022/day_6/solution.rs
@@ -1,10 +1,11 @@
-use std::collections::{VecDeque, HashMap};
+use std::{collections::{VecDeque, HashMap}, io::Read};
 
-use crate::{util::file::read_input, aoc_2022::constants};
+use crate::{util::file::get_input_reader, aoc_2022::constants::YEAR};
 
 const PROBLEM: &str = "day_6";
 
-const BUFFER_SIZE: usize = 4;
+pub const BUFFER_SIZE_START_OF_PACKET: usize = 4;
+pub const BUFFER_SIZE_START_OF_MESSAGE: usize = 14;
 
 fn eject_item(map: &mut HashMap<char, i32>, key: char) {
     let mut quantity = 0;
@@ -29,16 +30,19 @@ fn insert_item(map: &mut HashMap<char, i32>, key: char) {
 
 /// Tuning Trouble
 /// https://adventofcode.com/2022/day/6
-pub fn solve(filename: String) -> i32 {
-    let contents = read_input(filename, constants::YEAR.to_string(), PROBLEM.to_string());
+pub fn solve(filename: &str, buffer_size: usize) -> i32 {
+    let mut reader = get_input_reader(filename, YEAR, PROBLEM);
+    let mut contents: String = String::new();
+    _ = reader.read_to_string(&mut contents);
+
     let mut map: HashMap<char, i32> = HashMap::new();
     let mut buffer: VecDeque<char> = VecDeque::new();
 
     let mut idx = 0;
     let mut chars = contents.chars();
     while let Some(letter) = chars.next() {
-        if buffer.len() == BUFFER_SIZE {
-            if map.keys().len() == BUFFER_SIZE { break; }
+        if buffer.len() == buffer_size {
+            if map.keys().len() == buffer_size { break; }
             let element = buffer.pop_front();
             match element {
                 Some(x) => eject_item(&mut map, x),
@@ -53,15 +57,21 @@ pub fn solve(filename: String) -> i32 {
     return idx;
 }
 
-
 #[cfg(test)]
 mod tests {
-    use crate::aoc_2022::day_6::solution::solve;
+    use super::{solve, BUFFER_SIZE_START_OF_PACKET, BUFFER_SIZE_START_OF_MESSAGE};
 
     #[test]
-    fn test() {
+    fn part_1() {
         let input_file = "sample.txt";
-        let result = solve(input_file.to_string());
+        let result = solve(input_file, BUFFER_SIZE_START_OF_PACKET);
         assert_eq!(result, 1896);
+    }
+
+    #[test]
+    fn part_2() {
+        let input_file = "sample.txt";
+        let result = solve(input_file, BUFFER_SIZE_START_OF_MESSAGE);
+        assert_eq!(result, 3452);
     }
 }
