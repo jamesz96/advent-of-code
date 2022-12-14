419241c28133a32f113934d765049d151a8e4a34
jamesz96 <jamesz96.au@gmail.com>
2022-12-14 19:20:35 +1100

    problem 2 - part 2

--- src/aoc_2022/day_2/solution.rs
+++ src/aoc_2022/day_2/solution.rs
@@ -1,4 +1,8 @@
-use crate::util::file::read_input;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufReader, Error, BufRead};
+
+use crate::util::file::{read_input, get_current_dir};
 use crate::aoc_2022::constants;
 
 const PROBLEM: &str = "day_2";
@@ -7,8 +11,8 @@ const ROCK: usize = 0;
 const PAPER: usize = 1;
 const SCISSOR: usize = 2;
 
-#[derive(Clone, Copy, PartialEq)]
-enum Result {
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+pub enum PlayResult {
     LOSS,
     TIE,
     WIN,
@@ -23,25 +27,34 @@ pub fn transform(letter: &str) -> usize {
     }
 }
 
+pub fn transform_part_2(letter: &str) -> PlayResult {
+    match letter {
+        "X" => return PlayResult::LOSS,
+        "Y" => return PlayResult::TIE,
+        "Z" => return PlayResult::WIN,
+        _ => panic!("UNKNOWN character"),
+    }
+}
+
 /// Rock Paper Scissors
 /// https://adventofcode.com/2022/day/2
 pub fn solve(filename: String) -> i32 {
     let contents = read_input(filename, constants::YEAR.to_string(), PROBLEM.to_string());
     let lines: Vec<&str> = contents.split("\n").collect();
 
-    let mut result_table = [[Result::TIE; 3]; 3];
+    let mut result_table = [[PlayResult::TIE; 3]; 3];
 
-    result_table[ROCK][SCISSOR] = Result::LOSS;
-    result_table[ROCK][ROCK] = Result::TIE;
-    result_table[ROCK][PAPER] = Result::WIN;
+    result_table[ROCK][SCISSOR] = PlayResult::LOSS;
+    result_table[ROCK][ROCK] = PlayResult::TIE;
+    result_table[ROCK][PAPER] = PlayResult::WIN;
 
-    result_table[PAPER][ROCK] = Result::LOSS;
-    result_table[PAPER][PAPER] = Result::TIE;
-    result_table[PAPER][SCISSOR] = Result::WIN;
+    result_table[PAPER][ROCK] = PlayResult::LOSS;
+    result_table[PAPER][PAPER] = PlayResult::TIE;
+    result_table[PAPER][SCISSOR] = PlayResult::WIN;
 
-    result_table[SCISSOR][PAPER] = Result::LOSS;
-    result_table[SCISSOR][SCISSOR] = Result::TIE;
-    result_table[SCISSOR][ROCK] = Result::WIN;
+    result_table[SCISSOR][PAPER] = PlayResult::LOSS;
+    result_table[SCISSOR][SCISSOR] = PlayResult::TIE;
+    result_table[SCISSOR][ROCK] = PlayResult::WIN;
 
     let mut score = 0;
 
@@ -54,11 +67,11 @@ pub fn solve(filename: String) -> i32 {
         let mut round_score = (my_action as i32) + 1;
 
         let result = result_table[opp_action][my_action];
-        if result == Result::LOSS {
+        if result == PlayResult::LOSS {
             round_score += 0;
-        } else if result == Result::TIE {
+        } else if result == PlayResult::TIE {
             round_score += 3;
-        } else if result == Result::WIN {
+        } else if result == PlayResult::WIN {
             round_score += 6;
         }
         score += round_score;
@@ -67,15 +80,83 @@ pub fn solve(filename: String) -> i32 {
     return score;
 }
 
+fn make_map(entries: &[usize; 3]) -> HashMap<PlayResult, usize> {
+    let mut result: HashMap<PlayResult, usize> = HashMap::new();
+    result.insert(PlayResult::WIN, entries[0]);
+    result.insert(PlayResult::TIE, entries[1]);
+    result.insert(PlayResult::LOSS, entries[2]);
+    return result;
+}
+
+pub fn solve_part_2(filename: String) -> Result<i32, Error> {
+    let current_dir = get_current_dir();
+    let path = format!("{}/src/{}/{}/{}", current_dir, constants::YEAR.to_string(), PROBLEM.to_string(), filename);
+
+    let mut table: HashMap<usize, HashMap<PlayResult, usize>> = HashMap::new();
+
+    let rock_entries: HashMap<PlayResult, usize> = make_map(&[PAPER, ROCK, SCISSOR]);
+    let paper_entries: HashMap<PlayResult, usize> = make_map(&[SCISSOR, PAPER, ROCK]);
+    let scissor_entries: HashMap<PlayResult, usize> = make_map(&[ROCK, SCISSOR, PAPER]);
+
+    table.insert(ROCK, rock_entries);
+    table.insert(PAPER, paper_entries);
+    table.insert(SCISSOR, scissor_entries);
+
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    let mut score = 0;
+
+    for raw_line in reader.lines() {
+        match raw_line {
+            Ok(line) => {
+                if line == "" { continue; }
+                let values: Vec<&str> = line.split(" ").collect();
+                let opp_action = transform(values[0]);
+                let play_result = transform_part_2(values[1]);
+
+                let my_action = table
+                    .get(&opp_action).unwrap()
+                    .get(&play_result).unwrap();
+
+                let mut round_score = (*my_action as i32) + 1;
+
+                if play_result == PlayResult::LOSS {
+                    round_score += 0;
+                } else if play_result == PlayResult::TIE {
+                    round_score += 3;
+                } else if play_result == PlayResult::WIN {
+                    round_score += 6;
+                }
+                score += round_score;
+            },
+            Err(error) => panic!("{}", error),
+        }
+    }
+
+    return Ok(score);
+}
+
 
 #[cfg(test)]
 mod tests {
-    use crate::aoc_2022::day_2::solution::solve;
+    use super::solve;
+    use super::solve_part_2;
 
     #[test]
     fn test() {
         let input_file = "sample.txt";
         let result = solve(input_file.to_string());
         assert_eq!(result, 13565);
+    }
+
+    #[test]
+    fn part_2() {
+        let input_file = "sample.txt";
+        let result = solve_part_2(input_file.to_string());
+        match result {
+            Ok(x) => assert_eq!(x, 12424),
+            Err(_error) => return,
+        }
     }
 }
