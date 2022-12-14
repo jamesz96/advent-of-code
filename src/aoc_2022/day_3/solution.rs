3d802341844bbef6feef58ebbea51842633a0805
jamesz96 <jamesz96.au@gmail.com>
2022-12-14 22:37:20 +1100

    problem 3 - part 2

--- src/aoc_2022/day_3/solution.rs
+++ src/aoc_2022/day_3/solution.rs
@@ -1,8 +1,9 @@
 use std::collections::HashSet;
+use std::io::BufRead;
 
+use crate::aoc_2022::constants::YEAR;
 use crate::util::char::get_letter_idx;
-use crate::util::file::read_input;
-use crate::aoc_2022::constants;
+use crate::util::file::get_input_reader;
 
 const PROBLEM: &str = "day_3";
 const ALPHABET_LENGTH: u32 = 26;
@@ -16,44 +17,84 @@ fn get_priority_value(letter: char) -> u32 {
 
 /// Rucksack Reorganization
 /// https://adventofcode.com/2022/day/3
-pub fn solve(filename: String) -> i32 {
-    let contents = read_input(filename, constants::YEAR.to_string(), PROBLEM.to_string());
-    let lines: Vec<&str> = contents.split("\n").collect();
-
+pub fn solve_1(filename: &str) -> i32 {
+    let reader = get_input_reader(filename, YEAR, PROBLEM);
     let mut result = 0;
 
-    for line in lines {
-        if line == "" { continue }
-        let items: Vec<char> = line.chars().collect();
+    for raw_line in reader.lines() {
+        match raw_line {
+            Ok(line) => {
+                if line == "" { continue }
+                let items: Vec<char> = line.chars().collect();
 
-        let mut rucksack_a = HashSet::new();
-        let rucksack_size = items.len() / 2;
+                let mut rucksack_a = HashSet::new();
+                let rucksack_size = items.len() / 2;
 
-        for i in 0..rucksack_size {
-            rucksack_a.insert(items[i]);
-        }
+                for i in 0..rucksack_size {
+                    rucksack_a.insert(items[i]);
+                }
 
-        let mut prioritised_items = HashSet::new();
-        for i in 0..rucksack_size {
-            let item = items[rucksack_size+i];
-            if rucksack_a.contains(&item) && !prioritised_items.contains(&item) {
-                result += get_priority_value(item);
-                prioritised_items.insert(item);
-            }
-        }
+                let mut prioritised_items = HashSet::new();
+                for i in 0..rucksack_size {
+                    let item = items[rucksack_size+i];
+                    if rucksack_a.contains(&item) && !prioritised_items.contains(&item) {
+                        result += get_priority_value(item);
+                        prioritised_items.insert(item);
+                    }
+                }
+            },
+            Err(error) => panic!("{}", error)
+        };
     }
     return result as i32;
+}
+
+pub fn solve_2(filename: &str) -> i32 {
+    let reader = get_input_reader(filename, YEAR, PROBLEM);
+    let mut result = 0;
+
+    let mut iter = reader.lines();
+
+    while let Some(raw_line) = iter.next() {
+        match raw_line {
+            Ok(line) => {
+                if line == "" { continue; }
+                let line_b = iter.next().unwrap().unwrap();
+                let rucksack_b: HashSet<char> = line_b.chars().collect();
+
+                let line_c = iter.next().unwrap().unwrap();
+                let rucksack_c: HashSet<char> = line_c.chars().collect();
+
+                for elem in line.chars() {
+                    if rucksack_b.contains(&elem) && rucksack_c.contains(&elem) {
+                        result += get_priority_value(elem) as i32;
+                        break;
+                    }
+                }
+            },
+            Err(error) => panic!("{}", error)
+        };
+    }
+    return result;
 }
 
 
 #[cfg(test)]
 mod tests {
-    use crate::aoc_2022::day_3::solution::solve;
+    use super::solve_1;
+    use super::solve_2;
 
     #[test]
-    fn test() {
+    fn part_1() {
         let input_file = "sample.txt";
-        let result = solve(input_file.to_string());
+        let result = solve_1(input_file);
+        assert_eq!(result, 7428);
+    }
+
+    #[test]
+    fn part_2() {
+        let input_file = "sample.txt";
+        let result = solve_2(input_file);
         assert_eq!(result, 7428);
     }
 }
