1d0a24bbb7b3c26411fa0d73fa8f8ae54a51bdee
jamesz96 <jamesz96.au@gmail.com>
2022-12-21 21:13:18 +1100

    problem 13 - fixes

--- src/aoc_2022/day_13/solution.rs
+++ src/aoc_2022/day_13/solution.rs
@@ -1,112 +1,71 @@
-use std::{io::{BufReader, Error, BufRead}, fs::File};
-use crate::{aoc_2022::constants, util::file::get_current_dir};
+use std::{io::BufRead, cmp::Ordering};
+use crate::{aoc_2022::constants::YEAR, util::file::get_input_reader};
 
 const PROBLEM: &str = "day_13";
 
-#[derive(Debug)]
-enum Element {
+#[derive(PartialEq, Eq, Debug)]
+pub enum Packet {
     Value(i32),
-    Packet(Packet),
+    List(Vec<Packet>),
 }
 
-enum CompareResult {
-    Same,
-    InOrder,
-    OutOfOrder,
-}
-
-#[derive(Debug)]
-struct Packet {
-    elements: Vec<Element>,
-}
-
-fn is_in_order(a: &Packet, b: &Packet) -> CompareResult {
-    let mut idx_a = 0;
-    let mut idx_b = 0;
-
-    while idx_a < a.elements.len() && idx_b < b.elements.len() {
-        let element_a = &a.elements[idx_a];
-        let element_b = &b.elements[idx_b];
-
-        match element_a {
-            Element::Value(val_a) => {
-                match element_b {
-                    Element::Value(val_b) => {
-                        println!("-----");
-                        println!("{}", val_a);
-                        println!("{}", val_b);
-                        if val_a > val_b { return CompareResult::OutOfOrder }
-                        if val_a < val_b { return CompareResult::InOrder; }
-                    },
-                    Element::Packet(packet_b) => {
-                        let transformed_packet_a = Packet { elements: vec![Element::Value(*val_a)] };
-                        let result = is_in_order(&transformed_packet_a, packet_b);
-                        match result {
-                            CompareResult::Same => (),
-                            _ => return result,
-                        };
-                    },
-                };
-            },
-            Element::Packet(packet_a) => {
-                match element_b {
-                    Element::Value(val_b) => {
-                        let transformed_packet_b = Packet { elements: vec![Element::Value(*val_b)] };
-                        let result = is_in_order(packet_a, &transformed_packet_b);
-                        match result {
-                            CompareResult::Same => (),
-                            _ => return result,
-                        };
-                    },
-                    Element::Packet(packet_b) => {
-                        let result = is_in_order(packet_a, packet_b);
-                        match result {
-                            CompareResult::Same => (),
-                            _ => return result,
-                        }
-                    }
-                };
-            }
-        };
-
-        idx_a += 1;
-        idx_b += 1;
+impl Ord for Packet {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match (self, other) {
+            (Self::List(a), Self::List(b)) => a.cmp(b),
+            (Self::List(a), Self::Value(b)) => a.cmp(&vec![Packet::Value(*b)]),
+            (Self::Value(a), Self::Value(b)) => a.cmp(b),
+            (Self::Value(a), Self::List(b)) => vec![Packet::Value(*a)].cmp(&b),
+        }
     }
+}
 
-    if idx_a < a.elements.len() && idx_b == b.elements.len() {
-        return CompareResult::OutOfOrder;
+impl PartialOrd for Packet {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
-    return CompareResult::Same;
 }
 
 fn parse_packet(line: &str) -> Packet {
-    let mut root = Packet { elements: vec![] };
     let mut stack: Vec<Packet> = Vec::new();
+    let mut iter = line.chars().peekable();
 
-    for char in line.chars() {
+    let mut root = Packet::List(vec![Packet::Value(0)]);
+
+    while let Some(char) = iter.next() {
         match char {
             ',' => continue,
             '[' => {
-                let new_packet = Packet { elements: vec![] };
-                stack.push(new_packet);
+                let new_packet = vec![];
+                stack.push(Packet::List(new_packet));
             },
             ']' => {
                 let packet = stack.pop().unwrap();
                 match stack.last_mut() {
                     Some(parent) => {
-                        let packet_element = Element::Packet(packet);
-                        parent.elements.push(packet_element)
+                        match parent {
+                            Packet::List(x) => _ = x.push(packet),
+                            Packet::Value(_) => (),
+                        };
                     },
                     None => root = packet,
                 };
             },
             _ => {
                 let parent = stack.last_mut().unwrap();
-
-                let value = String::from(char).parse::<i32>().unwrap();
-                let value_element = Element::Value(value);
-
-                parent.elements.push(value_element);
+                match parent {
+                    Packet::Value(_) => (),
+                    Packet::List(list) => {
+                        let mut digits: Vec<char> = vec![char];
+                        while let Some(digit) = iter.peek() {
+                            if digit.is_digit(10) { digits.push(*digit); } else { break; }
+                            iter.next();
+                        }
+                        let number: String = digits.iter().collect();
+                        let value = String::from(number).parse::<i32>().unwrap();
+                        list.push(Packet::Value(value));
+                    }
+                }
             }
         }
     }
@@ -117,12 +76,8 @@ fn parse_packet(line: &str) -> Packet {
 
 /// Distress Signal
 /// https://adventofcode.com/2022/day/13
-pub fn solve(filename: String) -> Result<i32, Error> {
-    let current_dir = get_current_dir();
-    let path = format!("{}/src/{}/{}/{}", current_dir, constants::YEAR.to_string(), PROBLEM.to_string(), filename);
-
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
+pub fn solve(filename: &str) -> i32 {
+    let reader = get_input_reader(filename, YEAR, PROBLEM);
 
     let mut result = 0;
 
@@ -135,27 +90,16 @@ pub fn solve(filename: String) -> Result<i32, Error> {
                 let packet_a = parse_packet(&input);
                 let packet_b = parse_packet(&iter.next().unwrap().unwrap());
 
-                let order_result = is_in_order(&packet_a, &packet_b);
-                match order_result {
-                    CompareResult::OutOfOrder => {
-                        result += idx;
-                        println!("{}", false);
-                    },
-                    _ => println!("{}", true),
-                }
+                if packet_a < packet_b { result += idx; }
 
                 // Skip blank line
                 iter.next();
-
                 idx += 1;
             },
             Err(error) => panic!("{}", error),
         };
     }
-
-
-
-    return Ok(result);
+    return result;
 }
 
 
@@ -163,25 +107,17 @@ pub fn solve(filename: String) -> Result<i32, Error> {
 mod tests {
     use crate::aoc_2022::day_13::solution::solve;
 
-    #[ignore]
+    #[test]
+    fn part_1_test() {
+        let input_file = "test.txt";
+        let result = solve(input_file);
+        assert_eq!(result, 13);
+    }
+
     #[test]
     fn part_1() {
         let input_file = "sample.txt";
-        let result = solve(input_file.to_string());
-        match result {
-            Ok(x) => assert_eq!(x, 13),
-            Err(_error) => return,
-        }
-    }
-
-    #[ignore]
-    #[test]
-    fn test() {
-        let input_file = "test.txt";
-        let result = solve(input_file.to_string());
-        match result {
-            Ok(x) => assert_eq!(x, 13),
-            Err(_error) => return,
-        }
+        let result = solve(input_file);
+        assert_eq!(result, 5340);
     }
 }
